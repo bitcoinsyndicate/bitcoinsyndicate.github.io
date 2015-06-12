174767456c078f292dc1f5cd08a5775fbb50989f
bitcoinsyndicate <accounts@bitcoinsyndicate.net>
2015-06-12 13:01:04 +1000

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 71,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'Syndicate Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6LcoBggTAAAAANgjCApG8bedl4AD8ftBDJuegzts',  // <----- EDIT ME!
+  redirect_uri: 'https://bitcoinsyndicate.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
